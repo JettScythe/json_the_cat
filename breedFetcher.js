ff1c87bf7edf5d3988825abdd175f4447f9a23ba
JettScythe <jettscythe@hotmail.com>
2021-05-06 13:44:54 -0300

    It's kinda a mess for me to understand but it work

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,14 +1,18 @@
 const request = require('request');
-const myArgs = process.argv.slice(2);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${myArgs}`, (error, response, body) => {
-  if (error) {
-    throw error;
-  }
-  const data = JSON.parse(body);
-  if (data[0] === undefined) {
-    console.log(`${myArgs} is not a valid breed!`);
-  } else {
-    console.log(data[0].description);
-  }
-});
+const fetchBreedDescription = (breedName, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (!error) {
+      const data = JSON.parse(body);
+      if (data[0] === undefined) {
+        callback(`${breedName} is not a valid breed!`, null);
+      } else {
+        callback(null, data[0].description);
+      }
+    } else {
+      callback(error, null);
+    }
+  });
+};
+
+module.exports = fetchBreedDescription;
